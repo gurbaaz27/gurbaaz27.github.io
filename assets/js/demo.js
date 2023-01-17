b14ca317bfde5ab28f2ee55f24006846fffdb4d7
gurbaaz <gurbaazsn123@gmail.com>
2023-01-17 14:36:26 +0530

    Update links; remove site title

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,32 +1,10 @@
-// $(function() {
-//     $('#change-skin').on('click', function () {
-//       $("body").toggleClass("page-dark-mode");
-//       BeautifulJekyllJS.initNavbar();
-//     });
-//   });
 $(function() {
-    var theme;
-
-    if (localStorage.getItem('theme')) {  
-      theme = localStorage.getItem('theme');  
-    } else {  
-      theme = 'dark';  
-    }
-    console.log(theme);
-    if (theme == 'dark') {
-      $("body").toggleClass("page-dark-mode");
-    }
-
-    localStorage.setItem('theme', theme);
-
-    $('#change-skin').on('click', function () {
-      $("body").toggleClass("page-dark-mode");
-      BeautifulJekyllJS.initNavbar();
-
-      if (localStorage.getItem('theme') == 'dark') {
-        localStorage.setItem('theme', 'light'); 
-      } else {
-        localStorage.setItem('theme', 'dark');
-      }
-    });
+  $('#change-skin').on('click', function () {
+    $("body").toggleClass("page-dark-mode");
+    localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
+    BeautifulJekyllJS.initNavbar();
   });
+  if (localStorage.getItem('bj-dark-mode') === 'true') {
+    $('#change-skin').trigger('click');
+  }
+});
